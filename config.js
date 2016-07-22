ed68f39d64cec56226486eaa0a5bb3738045e3f8
Gerson Felipe Schwinn <gerson@onehost.com.br>
2016-07-22 16:46:44 -0300

    Atualizando

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var config = {
     portaServidor: 81,
     debug: true,
+    hostACBR: '192.168.1.102'
     //comandoImpressao: "/var/www/socketio/imprimir.sh-daruma",
-    pastaDestinoArquivoSerial: "bin/daruma/"
+    //pastaDestinoArquivoSerial: "bin/daruma/"
 };
 
 module.exports = config;
