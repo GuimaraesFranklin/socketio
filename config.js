60a5a2b8a1d4f93ed1576219fb2f43aabdf874f7
Gerson Felipe Schwinn <gerson@onehost.com.br>
2016-07-22 14:04:42 -0300

    Simplificando servidor, removendo parte do arquivo ACBR. Usando somente sockets

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 var config = {
-    //arquivoEntradaACBR: 'C:/ACBrMonitor/ENT.TXT',
-    //arquivoSaidaACBR: 'C:/ACBrMonitor/SAI.TXT',
-    //pastaACBR: 'C:/ACBrMonitor',
-    usarACBR: false,
     portaServidor: 81,
-    usarSockets: true,
     debug: true,
-    caminhoImpressoraSerial: "LPT1",
-    usarIconv: false,
     //comandoImpressao: "/var/www/socketio/imprimir.sh-daruma",
     pastaDestinoArquivoSerial: "bin/daruma/"
 };
