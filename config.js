93c4f0af6539544287e96d786495674e14e8af39
Gerson Felipe Schwinn <gerson@onehost.com.br>
2016-08-08 08:02:37 -0300

    Atualizando

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ var config = {
     portaServidor: 81,
     debug: true,
     hostACBR: '127.0.0.1',
+    executarShell: false
     //comandoImpressao: "C:/onehost/socketio/imprimir.bat"
-    //pastaDestinoArquivoSerial: "bin/daruma/"
+    //pastaDestinoArquivoSerial: "bin/daruma/",
 };
 
 module.exports = config;
