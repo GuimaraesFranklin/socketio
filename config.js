49ea9d781724b95ff22b9783be9ad750c07bc55b
Gerson Felipe Schwinn <gerson@onehost.com.br>
2017-08-14 14:31:37 -0300

    Iniciando pinpad

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 var config = {
-    portaServidor: 81,
+    portaServidor: 8081,
     habilitarSSL: true,
-    portaServidorSSL: 443,
+    portaServidorSSL: 8081,
     debug: true,
     hostACBR: '127.0.0.1',
     executarShell: false,
+    habilitarSuporteSitef: true,
+    pastaDestinoArquivosSitef: '/var/www/socketio/tmp/',
+    //pastaDestinoArquivosSitef: 'C:/Cliente/Req/',
     //comandoImpressao: "/var/www/socketio/imprimir.sh"
     comandoImpressao: "C:/onehost/socketio/imprimir.bat"
     //pastaDestinoArquivoSerial: "bin/daruma/",
