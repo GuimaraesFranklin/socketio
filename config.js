321668c33da3c4396ef5d15965bf32ac00bb952b
Gerson Felipe Schwinn <gerson@onehost.com.br>
2017-08-16 18:08:52 -0300

    Adicionando alterações

--- config.js
+++ config.js
@@ -3,11 +3,15 @@ var config = {
     habilitarSSL: true,
     portaServidorSSL: 8081,
     debug: true,
+    sitef: {
+        habilitar: true,
+        debug: true,
+        pastaDestino: '/var/www/socketio/tmp/',
+        //pastaDestino: 'C:/Cliente/Req/',
+    },
+    
     hostACBR: '127.0.0.1',
     executarShell: false,
-    habilitarSuporteSitef: true,
-    pastaDestinoArquivosSitef: '/var/www/socketio/tmp/',
-    //pastaDestinoArquivosSitef: 'C:/Cliente/Req/',
     //comandoImpressao: "/var/www/socketio/imprimir.sh"
     comandoImpressao: "C:/onehost/socketio/imprimir.bat"
     //pastaDestinoArquivoSerial: "bin/daruma/",
