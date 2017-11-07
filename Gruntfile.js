0e52b80d9ff97399ba5fec0145642b3a0e5fc68f
Gerson Felipe Schwinn <gerson@onehost.com.br>
2017-11-07 15:25:21 -0200

    Atualizndo Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function (grunt) {
                     {
                         src: [
                             'bin/**',
-                            'certs/**',
                             '!node_modules/**',
                             'package.json',
                             'README.md',
