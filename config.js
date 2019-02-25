68ec6c67d4c1cc55d4168786b24d7601e43b34b4
Gerson Felipe Schwinn <gerson@onehost.com.br>
2019-02-25 10:27:25 -0300

    Atualizando configuração

--- config.js
+++ config.js
@@ -1,23 +1,24 @@
 var config = {
-    portaServidor: 81,
-    habilitarSSL: true,
-    portaServidorSSL: 8082,
-    debug: true,
-    pastaBase: __dirname,
-    pastaLogs: __dirname + '/logs/',
-    sitef: {
-        habilitar: true,
-        debug: false,
-        //pastaDestinoReq: '/var/www/socketio/tmp/Req/',
-        pastaDestinoReq: 'C:/Client/Req/',
-        //pastaDestinoResp: '/var/www/socketio/tmp/Resp/',
-        pastaDestinoResp: 'C:/Client/Resp/',
-        pastaArquivosExemplo: 'C:/onehost/socketio/'
+    "portaServidor": 81,
+    "habilitarSSL": true,
+    "portaServidorSSL": 8082,
+    "debug": true,
+    "pastaBase": __dirname,
+    "pastaLogs": __dirname + '/logs/',
+    "sitef": {
+        "habilitar": true,
+        "debug": true,
+        "pastaDestinoReq": '/var/www/socketio/tmp/Req/',
+        //"pastaDestinoReq": 'C:/Client/Req/',
+        "pastaDestinoResp": '/var/www/socketio/tmp/Resp/',
+        //"pastaDestinoResp": 'C:/Client/Resp/',
+        "pastaArquivosExemplo": 'C:/onehost/socketio/'
     },
-    hostACBR: '127.0.0.1',
-    executarShell: false,
-    //comandoImpressao: "/var/www/socketio/imprimir.sh"
-    comandoImpressao: "C:/onehost/socketio/imprimir.bat"
+    "hostACBR": "127.0.0.1",
+    "portaACBR": 3434,
+    "executarShell": false,
+    "comandoImpressao": "/var/www/socketio/imprimir.sh"
+    //comandoImpressao: "C:/onehost/socketio/imprimir.bat"
     //pastaDestinoArquivoSerial: "bin/daruma/",
 };
 
